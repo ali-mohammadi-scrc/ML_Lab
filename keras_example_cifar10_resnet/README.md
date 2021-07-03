4025113af61f17010ef43c13299dc812bd734a1d
Ali Mohammadi <65521817+ali-mohammadi-scrc@users.noreply.github.com>
2021-07-03 23:55:41 +0200

    keras example - ResNet CIFAR10

---
+++ keras_example_cifar10_resnet/README.md
@@ -0,0 +1,5 @@
+# ResNet model in Tensorflow
+
+An implementation of ResNet model in Tensorflow by [Keras-team](https://github.com/keras-team) with slight modifications.
+
+Please see [here](https://keras.io/zh/examples/cifar10_resnet/) or [here](https://github.com/keras-team/keras-docs-zh/blob/master/sources/examples/cifar10_resnet.md) for the original implementation.
---
+++ keras_example_cifar10_resnet/cifar10.py
@@ -0,0 +1,27 @@
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+
+
+def load_cifar10(subtract_pixel_mean=True):
+    # Load the CIFAR10 data.
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    # Input image dimensions.
+    input_shape = x_train.shape[1:]
+
+    # Normalize data.
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    # If subtract pixel mean is enabled
+    if subtract_pixel_mean:
+        x_train_mean = np.mean(x_train, axis=0)
+        x_train -= x_train_mean
+        x_test -= x_train_mean
+
+    # Convert class vectors to binary class matrices.
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
+
+    return (x_train, y_train), (x_test, y_test)
