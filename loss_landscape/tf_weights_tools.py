c0a4209b7748f55538578113ef10a8cfff5608df
Ali Mohammadi <65521817+ali-mohammadi-scrc@users.noreply.github.com>
2021-08-29 13:51:23 +0200

    Update tf_weights_tools.py

--- loss_landscape/tf_weights_tools.py
+++ loss_landscape/tf_weights_tools.py
@@ -38,3 +38,33 @@ def model_weights_to_weights_by_layer(model, weights):
         weights_by_layer.append(weights[start:start+n_layer_weights])
         start += n_layer_weights
     return weights_by_layer
+
+
+def get_layers_weights1D(model, layers_indices):
+    layers_weights = []
+    for i in layers_indices:
+        for weights in model.layers[i].get_weights():
+            if np.ndim(weights) > 1:  # Kernel weights
+                layers_weights.append(np.ndarray.flatten(weights))
+    return np.concatenate(layers_weights)
+
+
+def set_layers_weights(model, layers_indices, weights1D, n=2):
+    start = 0
+    for i in layers_indices:
+        new_weights = []
+        for weights in model.layers[i].get_weights():
+            if np.ndim(weights) > 1:  # Kernel weights
+                if n is None:
+                    end = start + weights.size
+                    new_weights.append(np.reshape(
+                        weights1D[start:end], newshape=weights.shape))
+                    start = end
+                else:
+                    w = np.ndarray.flatten(weights)
+                    w[:n] = weights1D[start:start+n]
+                    new_weights.append(np.reshape(w, newshape=weights.shape))
+                    start += n
+            else:
+                new_weights.append(weights)
+        model.layers[i].set_weights(new_weights)
