21bca60f67aa9c7ebc47872f0fc6309d61bc8672
Ali Mohammadi <65521817+ali-mohammadi-scrc@users.noreply.github.com>
2021-08-29 13:59:26 +0200

    Update cifar10.py

--- keras_example_cifar10_resnet/cifar10.py
+++ keras_example_cifar10_resnet/cifar10.py
@@ -3,12 +3,9 @@ from tensorflow.keras.utils import to_categorical
 import numpy as np
 
 
-def load_cifar10(subtract_pixel_mean=True):
+def load_cifar10(subtract_pixel_mean=True, return_part='both'):
     # Load the CIFAR10 data.
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-    # Input image dimensions.
-    input_shape = x_train.shape[1:]
 
     # Normalize data.
     x_train = x_train.astype('float32') / 255
@@ -24,4 +21,8 @@ def load_cifar10(subtract_pixel_mean=True):
     y_train = to_categorical(y_train, 10)
     y_test = to_categorical(y_test, 10)
 
+    if return_part == 'train':
+        return x_train, y_train
+    elif return_part == 'test':
+        return x_test, y_test
     return (x_train, y_train), (x_test, y_test)
